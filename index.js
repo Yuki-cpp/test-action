c6078b1d864c4e4ea991c1704afc62c5fea08c62
Leo <35804463+Yuki-cpp@users.noreply.github.com>
2022-06-26 12:57:37 +0900

    eee

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const shell = require('shelljs')
+
+
+
+
+
+
+
+
+
+
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    github.context.actor
+    const clone_command = `git clone https://${username}:${token}@github.com/${username}/${repo}.git`
+
+
+
+    const path = 'absolute/path/to/folder'
+    shell.cd(path)
+    shell.exec('git clone https://github.com/atomicptr/dauntless-builder')
+
+
+
+
+
+
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = github.context.actor;
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
